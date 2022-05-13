231be7a0fd1e032d67d84f705061b69553560e5c
VivianKuKu <v830108ku543@gmail.com>
2022-05-14 00:36:21 +0100

    QEOP IoT

---
+++ QEOP IoT/ios/Runner/AppDelegate.swift
@@ -0,0 +1,20 @@
+import UIKit
+import Flutter
+import GoogleMaps  // Add this import
+
+
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+
+    // TODO: Add your Google Maps API key
+    GMSServices.provideAPIKey("Your API")
+
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
